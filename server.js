3c431445aa041f3ca8466104051540d6df4d40a8
AyushiDhupia <ayushi.dhupia@cognizant.com>
2017-06-20 07:44:37 +0000

    20 June2017

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose=require('mongoose');
 var app = express();
+var User=mongoose.model('User', { 
+ username: String, password: String  });
+ var bcrypt = require('bcryptjs');
+ var jwt = require('jwt-simple');
+ var JWT_SECRET='catsmeow';
+
 app.use(bodyParser.json());
 mongoose.connect('mongodb://localhost:27017/mittens');
 app.use(express.static('public')); 
@@ -30,6 +36,35 @@ var meowId=req.body.meow._id;
 			return res.send("Deleted Successfully");
 	});
 });
+
+app.put('/users/signin',function(req,res,next){
+	User.findOne({username:req.body.username},function(err,user){
+		bcrypt.compare(req.body.password,user.password,function(err,result){
+			if(result){
+			var token=jwt.encode(user,JWT_SECRET);
+			return res.json({token:token});
+		}
+		else{
+			return res.status(400).send();
+				}
+		});
+	});
+});
+
+app.post('/users', function(req, res, next){
+		bcrypt.genSalt(10, function(err,salt){
+		bcrypt.hash(req.body.password,salt,function(err,hash){
+	
+			var newUser= new User({
+				username: req.body.username,
+				password: hash
+			});
+			newUser.save( function(err){
+			return res.send();
+	});		
+}) ;
+		});		
+}) ;
 
 app.listen(4000, function () {
   console.log('Example app listening on port 4000!')
